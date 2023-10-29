d2ce48090bc5f9004b790652c003a9bafe47018a
ateals <ateals@icloud.com>
2023-10-29 20:21:47 +0900

    init: CNA

---
+++ src/app/layout.tsx
@@ -0,0 +1,9 @@
+import "@/styles/globals.css";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
